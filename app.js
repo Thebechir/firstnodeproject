cabeb894cdd4f396627f24bf67981854e062fdba
Thebechir <bechirnaouar2@gmail.com>
2021-04-06 14:58:14 +0100

    first boy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const connect = require('./database/connect')
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
